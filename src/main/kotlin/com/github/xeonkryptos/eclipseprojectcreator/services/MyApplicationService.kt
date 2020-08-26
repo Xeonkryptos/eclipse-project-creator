d28ce191891c28d707f663b035839502169c1d3c
GitHub Action <action@github.com>
2020-08-26 06:13:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xeonkryptos/eclipseprojectcreator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xeonkryptos.eclipseprojectcreator.services
+
+import com.github.xeonkryptos.eclipseprojectcreator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/xeonkryptos/eclipseprojectcreator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xeonkryptos/eclipseprojectcreator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.xeonkryptos.eclipseprojectcreator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xeonkryptos.eclipseprojectcreator.MyBundle
 
 class MyProjectService(project: Project) {
 
